ab10c92155f89b6d52942754a5922f2369d9e5e8
RossPerot <rmh8980@g.rit.edu>
2017-10-06 14:16:31 -0400

    fixed collision

--- src/server.js
+++ src/server.js
@@ -31,14 +31,26 @@ const checkCollisions = (socket) => {
         if (square1.x < square2.x + square2.width && square1.x + square1.width > square2.x &&
             square1.y < square2.y + square2.height && square1.height + square1.y > square2.y) {
           if (square1.seeker === true) {
-            square2.x = Math.floor(Math.random() * 700);
-            square2.y = Math.floor(Math.random() * 700);
+            const newX = Math.floor(Math.random() * 700);
+            const newY = Math.floor(Math.random() * 700);
+            square2.x = newX;
+            square2.y = newY;
+            square2.prevX = newX;
+            square2.prevY = newY;
+            square2.destX = newX;
+            square2.destY = newY;
             square1.seeker = false;
             const key = keys[(Math.floor(Math.random() * keys.length))];
             squares[key].seeker = true;
           } else if (square2.seeker === true) {
-            square1.x = Math.floor(Math.random() * 700);
-            square1.y = Math.floor(Math.random() * 700);
+            const newX = Math.floor(Math.random() * 700);
+            const newY = Math.floor(Math.random() * 700);
+            square1.x = newX;
+            square1.y = newY;
+            square1.prevX = newX;
+            square1.prevY = newY;
+            square1.destX = newX;
+            square1.destY = newY;
             square2.seeker = false;
             const key = keys[(Math.floor(Math.random() * keys.length))];
             squares[key].seeker = true;
@@ -54,19 +66,19 @@ io.on('connection', (sock) => {
   const socket = sock;
   socket.join('room1');
 
-    var startX = Math.floor(Math.random() * 700);
-    var startY = Math.floor(Math.random() * 700);
-    
+  const startX = Math.floor(Math.random() * 700);
+  const startY = Math.floor(Math.random() * 700);
+
   socket.square = {
     hash: xxh.h32(`${socket.id}${new Date().getTime()}`, 0xCAFEBABE).toString(16),
     lastUpdate: new Date().getTime(),
     x: startX,
     y: startY,
-    prevX: startX, 
-    prevY: startY, 
-    destX: startX, 
-    destY: startY, 
-    alpha: 0, 
+    prevX: startX,
+    prevY: startY,
+    destX: startX,
+    destY: startY,
+    alpha: 0,
     height: 50,
     width: 50,
     seeker: false,
